a84c517067629f9ecfb0b7ee18801237fa93b717
SabirovI <insaf.sabirov.02@mail.ru>
2020-12-22 11:47:48 +0300

    finish

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -6,8 +6,34 @@ namespace Exam
     {
         static void Main(string[] args)
         {
-            int[,] nums = new int[10, 10];
+            int[,] matrix = new int[10, 10];
+            int[,] sumnums = new int[10, 10];
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                 
+                    for matrix1[i, j] = rnd.Next(0, 100)
+                }
+
+            }
+            int summ = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i == j) ;
+                    {
+                        summ += matrix[i, j];
+                    }
+                }
+                Console.WriteLine(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
 
         }
+        Console.WriteLine($"Сумма элемнтов равна: {summ}");
     }
+
 }
