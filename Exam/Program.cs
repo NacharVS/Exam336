d9fa331ffa19d31db31daa825884aee8fd2cf6dc
AndreySapegin336 <andreysapegin145@gmail.com>
2020-12-22 10:00:14 +0300

    finish

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -2,11 +2,71 @@
 
 namespace Exam
 {
+
+    interface Damage
+    {
+        public string SetName(string name)
+        {
+            return name;
+        }
+
+        public int SetPower(int power)
+        {
+            return power;
+        }
+
+        public int SetLuck(int luck)
+        {
+            return luck;
+        }
+
+        public int SetLovk(int lovk)
+        {
+            return lovk;
+        }
+    }
+    class Person : Damage
+    {
+
+        public void SetAct()
+        {
+            int act;
+            Console.WriteLine("Совершается действие:");
+            Random rnd = new Random();
+            act = rnd.Next();
+        }
+
+       
+
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person pers = new Person(); 
+            Damage person = new Person();
+            Console.WriteLine("Введите имя персонажа");
+             
+            person.SetName(Console.ReadLine());
+           
+            Console.WriteLine("Введите силу персонажа");
+
+            person.SetPower(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Введите удачу персонажа");
+
+            person.SetLuck(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Введите ловкость персонажа");
+
+            person.SetLovk(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Введите имя персонажа");
+            
+            person.Set
+
         }
     }
 }
