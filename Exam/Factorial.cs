1f7ef0cda61eae5551f587d88ab389fa097ced81
Sogorin <Sogorin_ivan@mail.ru>
2020-12-22 10:34:51 +0300

    Factorial

---
+++ Exam/Factorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam
+{
+    class Factorial
+    {
+        static int factorial(int n)
+        {
+
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+
+        }
+        
+    }
+}
