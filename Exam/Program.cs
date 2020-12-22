0f51f0e87205d075d46ba93bf11774a1ef8be95b
AndreySapegin336 <andreysapegin145@gmail.com>
2020-12-22 10:31:32 +0300

    Loginov

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -4,9 +4,74 @@ namespace Exam
 {
     class Program
     {
+
+
+        public static void GenerationArray(int[] arr, int MinN, int MaxN)
+        {
+            Console.WriteLine("sgr mas");
+            Random rnd = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                arr[i] = rnd.Next(MinN, MaxN);
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            return;
+        }
+
+        private static void SumArray(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                sum = sum + arr[i];
+                Console.WriteLine($"Сумма - {sum} ");
+            }
+
+            return;
+        }
+        static void SortArray(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int n = i + 1; n < arr.Length; n++)
+                {
+                    if (arr[i] > arr[n])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[n];
+                        arr[n] = temp;
+                    }
+                }
+            }
+            return;
+        }
+        private static void PrArr(int[] arr)
+        {
+            int pr = arr[1];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                pr = pr * arr[i];
+            }
+            Console.WriteLine($"Произведение - {pr} ");
+            return;
+        }
+
+
+        delegate void ObrMas(int[] Arr);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] Arr = new int[10];
+            GenerationArray(Arr, 0, 10);
+            ObrMas del;
+            del = SumArray;
+            del = PrArr;
+            del = SortArray;
+            del(Arr);
         }
+
+
+
     }
 }
